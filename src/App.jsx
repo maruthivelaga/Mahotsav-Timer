8e3827a1d2a97a78f9407e20586072186b8f715f
maruthivelaga <maruthivelaga6@gmail.com>
2025-12-09 09:36:33 +0530

    Fix image imports for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Preloader from './components/Preloader';
 import Timer from './components/Timer';
 import './App.css';
+import bgImage from './bg.avif';
+import mahotsavImage from './mahotsav.avif';
+import flowerImage from './flower.avif';
+import vignanLogoImage from './vignan-logo.avif';
 
 function App() {
   return (
     <>
       <Preloader />
       <div className="main-container">
-        <div className="background"></div>
-        <div className="gold"></div>
-        <div className="flower"></div>
-        <div className="vignan-logo"></div>
+        <div className="background" style={{ backgroundImage: `url(${bgImage})` }}></div>
+        <div className="gold" style={{ backgroundImage: `url(${mahotsavImage})` }}></div>
+        <div className="flower" style={{ backgroundImage: `url(${flowerImage})` }}></div>
+        <div className="vignan-logo" style={{ backgroundImage: `url(${vignanLogoImage})` }}></div>
         <Timer />
       </div>
     </>
